d65f8c50e3bb9c1ad9658ebab944610c9648b579
KAH20whj <56511679+KAH20whj@users.noreply.github.com>
2020-09-23 10:50:41 +0900

    Add files via upload

---
+++ first.js
@@ -0,0 +1,10 @@
+/* jslint devel: true */
+/* eslint-disable no-console */
+/* eslint no-undef: "error" */
+/* eslint-env node */
+/* eslint-env browser */
+
+function viewCurDate() { // eslint-disable-line no-unused-vars
+    "use strict";
+    document.getElementById('demo').innerHTML = Date();
+}
